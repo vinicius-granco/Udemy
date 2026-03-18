c2ac14aaf6e45960de9ea0db27055eab072a3537
vinicius-granco <viniciusftza@gmail.com>
2026-03-18 16:08:04 -0300

    feat: initialize Django project structure with basic settings, URLs, and WSGI/ASGI configurations
    - Created project directory and initialized __init__.py
    - Added settings.py with essential configurations
    - Set up URLs for the project and included recipes app
    - Implemented WSGI and ASGI configurations for deployment
    - Created initial migrations for Recipe and Category models
    - Added templates for category page and recipe display
    - Developed a factory for generating fake recipe data using Faker

--- Django/projeto1/project/urls.py
+++ Django/projeto1/project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for projeto project.
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from recipes.views import home
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
     path('', include('recipes.urls')),
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Django/projeto1/recipes/urls.py
+++ Django/projeto1/recipes/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
 from django.urls import path
+
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
-    
-    
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
